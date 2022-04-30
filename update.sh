fbecaaa7d54cef30976d4006164b265c1ebd7046
Yobi Bina Setiawan <yobi.studio@gmail.com>
2022-05-01 03:14:46 +0700

    add update sh

---
+++ update.sh
@@ -0,0 +1,9 @@
+composer dump-autoload
+ 
+php artisan migrate
+php artisan migrate --path=database/migrations/base/ --force
+
+php artisan passport:install
+php artisan key:generate
+
+php artisan storage:link
