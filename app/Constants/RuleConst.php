ca1424e5defa5bff4f7105096d3aded943c98263
Yobi Bina Setiawan <yobi.studio@gmail.com>
2022-05-12 10:50:10 +0700

    add ruleconstant

---
+++ app/Constants/RuleConst.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Constants;
+
+class RuleConst
+{
+
+    const MAX_STRING = 'max:200';
+    const MAX_TEXT = 'max:1000';
+    const MAX_FILE = 'max:5000';
+    const MIME_FILE = 'mimes:jpeg,jpg,png';
+
+}
